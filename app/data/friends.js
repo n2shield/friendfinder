23cbca11953f97280c71e9be45c06da33891554e
Chris Morrissey <chrismorrissey1985@gmail.com>
2018-04-28 09:20:42 -0400

    final

---
+++ app/data/friends.js
@@ -0,0 +1,36 @@
+var friendsArray = [
+{
+    "name": "Chris",
+    "photo": 'https://qph.fs.quoracdn.net/main-qimg-a790e9e4f418e04a45feb3a524793389',
+    'scores': [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+},
+{
+    "name": "Princess",
+    "photo": 'https://valleygirl.global.ssl.fastly.net/pub/media/catalog/product/cache/small_image/300x450/beff4985b56e3afdbeabfc89641a4582/h/t/httpswww.valleygirl.com.aupubmediaimport309100_crd_1.jpg',
+    'scores': [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        1,
+        1,
+        1,
+        1
+    ]
+
+}
+]
+module.exports = friendsArray;
